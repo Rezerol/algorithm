a50a2629178894c8c511cb964459aa68e1d51e77
卢光明 <luguangming@luguangmingdeMacBook-Pro.local>
2021-05-11 23:45:21 +0800

    fix bug in binary search

--- BinarySearch/BinarySearch.h
+++ BinarySearch/BinarySearch.h
@@ -4,7 +4,8 @@ int binary_search(T arr[], int n, int target)
     int l = 0, r = n - 1;   //find target in [l, r]
     while(l <= r)
     {
-        int mid = (l + r) / 2;
+        //int mid = (l + r) / 2;  //l+r may out of the range of int
+        int mid = l + (r-l)/2; 
         if(arr[mid] == target)
         {
             return mid;
